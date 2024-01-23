70d71589dde2a1cd9911c00bc35462661e674621
AdrienneUsi <usiadrienne35@gmail.com>
2024-01-23 10:09:34 -0800

    did exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		public int Year { set; get; }
+		public string Make { set; get; }
+		public string Model { set; get; }
+		public string EngineNoise { set; get; }
+		public string HonkNoise { set; get; }
+		public bool IsDrivable { get; set; }
+
+		public void MakeEngineNoise(string engineNoise)
+		{
+			EngineNoise = engineNoise;
+
+			Console.WriteLine($"{Make} {Model} has an engine noise that sounds like {EngineNoise}");
+		}
+
+		public void MakeHonkNoise(string honkNoise)
+		{
+			HonkNoise = honkNoise;
+
+
+
+          Console.WriteLine($"{Make} {Model} has an honk noise that sounds like {HonkNoise}.");
+
+        }
+	}
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,33 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CarLotSimulator
 {
-    class Program
+    public class CarLot
     {
-        static void Main(string[] args)
+        //Field
+        public static int _numberOfCars;
+
+
+        //Create a CarLot class
+        //It should have at least one property: a List of cars
+        //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+        //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+        public List<Car> ParkingLot { get; set; } = new List<Car>();
+
+        public void CheckCars()
         {
-            //TODO
-
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
-
-
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
-
-            //*************BONUS*************//
-
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
-
-            //*************BONUS X 2*************//
-
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach (var car in ParkingLot)
+            {
+                Console.WriteLine($"This is a {car.Make} {car.Model} made in the year {car.Year}");
+                car.MakeEngineNoise(car.EngineNoise);
+                car.MakeHonkNoise($"{car.HonkNoise}");
+            }
         }
     }
 }
